4c43541e36437d63bf1cadd9d1566a51fc1dca7b
sekiya9311 <sekiya9311@gmail.com>
2018-10-14 22:54:47 +0900

    add IEnumerable<T> extention methods

---
+++ UtilLibrary.Test/IEnumerableExtentionsTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UtilLibrary.Test
+{
+    using System.Linq;
+    using UtilLibrary;
+
+    public class IEnumerableExtentionsTest
+    {
+        [Fact]
+        public void PushFrontTest()
+        {
+            IEnumerable<int> foo = new int[] { 0 };
+            {
+                var bar = foo.PushFront(1);
+                Assert.True(bar.SequenceEqual(new int[] { 1, 0 }));
+                Assert.False(bar.SequenceEqual(new int[] { 0, 1 }));
+            }
+            {
+                var bar = foo.PushFront(1).PushFront(2).PushFront(3);
+                Assert.True(bar.SequenceEqual(new int[] { 3, 2, 1, 0 }));
+                Assert.False(bar.SequenceEqual(new int[] { 0, 1, 2, 3 }));
+            }
+        }
+
+        [Fact]
+        public void PushBackTest()
+        {
+            IEnumerable<int> foo = new int[] { 0 };
+            {
+                var bar = foo.PushBack(1);
+                Assert.True(bar.SequenceEqual(new int[] { 0, 1 }));
+                Assert.False(bar.SequenceEqual(new int[] { 1, 0 }));
+            }
+            {
+                var bar = foo.PushBack(1).PushBack(2).PushBack(3);
+                Assert.True(bar.SequenceEqual(new int[] { 0, 1, 2, 3 }));
+                Assert.False(bar.SequenceEqual(new int[] { 3, 2, 1, 0 }));
+            }
+        }
+    }
+}
---
+++ UtilLibrary/IEnumerableExtentions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilLibrary
+{
+    public static class IEnumerableExtentions
+    {
+        public static IEnumerable<TSource> PushFront<TSource>(this IEnumerable<TSource> source, TSource element)
+        {
+            yield return element;
+            foreach (var e in source)
+            {
+                yield return e;
+            }
+        }
+
+        public static IEnumerable<TSource> PushBack<TSource>(this IEnumerable<TSource> source, TSource element)
+        {
+            foreach (var e in source)
+            {
+                yield return e;
+            }
+            yield return element;
+        }
+    }
+}
